3198bb276cb41f724edb5d5990e642bb4434c57e
AdiByte30 <adityaandhale30@gmail.com>
2025-03-11 10:01:43 +0530

    changes for publlishing

--- lib/src/math_utils.dart
+++ lib/src/math_utils.dart
@@ -1,4 +1,4 @@
 class MathUtils {
-  static int add(int a,int b)=>a+b;
-  static int multiply(int a,int b)=>a*b;
+  static int add(int a, int b) => a + b;
+  static int multiply(int a, int b) => a * b;
 }
